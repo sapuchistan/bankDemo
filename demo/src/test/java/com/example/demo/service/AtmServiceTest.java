cdc1b45b37ecffdc3dc5b1674c805131d118bb91
sapuchistan <sapuchistan@hotmail.com>
2023-07-31 12:39:48 -0500

    Fix mvn test issues
    Updated to Junit 5

--- demo/src/test/java/com/example/demo/service/AtmServiceTest.java
+++ demo/src/test/java/com/example/demo/service/AtmServiceTest.java
@@ -1,14 +1,17 @@
 package com.example.demo.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.core.account.Account;
@@ -17,8 +20,8 @@ import com.example.demo.core.account.AccountRepo;
 import com.example.demo.core.customer.Customer;
 import com.example.demo.core.movement.MovementRepo;
 
-@RunWith(SpringRunner.class)
-public class AtmServiceTest
+@ExtendWith(SpringExtension.class)
+class AtmServiceTest
 {
 
   @InjectMocks
@@ -60,31 +63,40 @@ public class AtmServiceTest
     assertThat(value).isEqualTo(new BigDecimal("0.00"));
   }
 
-  @Test(expected = ResponseStatusException.class)
+  @Test
   public void testwithdraw_NotEnoughFund() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     AccountNumber accountNumber = new AccountNumber(1L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber, new BigDecimal("100.50"));
-    atmService.withDraw(account1, new BigDecimal("200.50"), false);
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.withDraw(account1, new BigDecimal("200.50"), false);
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 
-  @Test(expected = ResponseStatusException.class)
-  public void testwithdraw_NegativeValues() {
+  @Test
+  void testwithdraw_NegativeValues() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     AccountNumber accountNumber = new AccountNumber(1L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber, new BigDecimal("100.50"));
-    atmService.withDraw(account1, new BigDecimal("-200.50"), false);
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.withDraw(account1, new BigDecimal("-200.50"), false);
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 
-  @Test(expected = ResponseStatusException.class)
+  @Test
   public void testDeposit_NegativeValues() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     AccountNumber accountNumber = new AccountNumber(1L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber, new BigDecimal("0.0"));
-    atmService.deposit(account1, new BigDecimal("-100.50"), false);
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.deposit(account1, new BigDecimal("-100.50"), false);
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 
-  @Test(expected = ResponseStatusException.class)
+  @Test
   public void testTransfer_NegativeValues() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     Customer customer2 = new Customer(1L, "Elmer", "Fudd");
@@ -92,10 +104,13 @@ public class AtmServiceTest
     AccountNumber accountNumber2 = new AccountNumber(2L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber1, new BigDecimal("100.50"));
     Account account2 = new Account(2L, customer2, 51L, 1L, 101L, accountNumber2, new BigDecimal("0.00"));
-    atmService.transfer(account1, account2, new BigDecimal("-100.50"));
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.transfer(account1, account2, new BigDecimal("-100.50"));
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 
-  @Test(expected = ResponseStatusException.class)
+  @Test
   public void testTransfer_NotEnoughFund() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     Customer customer2 = new Customer(1L, "Elmer", "Fudd");
@@ -103,14 +118,20 @@ public class AtmServiceTest
     AccountNumber accountNumber2 = new AccountNumber(2L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber1, new BigDecimal("100.50"));
     Account account2 = new Account(2L, customer2, 51L, 1L, 101L, accountNumber2, new BigDecimal("0.00"));
-    atmService.transfer(account1, account2, new BigDecimal("200.50"));
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.transfer(account1, account2, new BigDecimal("200.50"));
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 
-  @Test(expected = ResponseStatusException.class)
+  @Test
   public void testTransfer_SameAccount() {
     Customer customer1 = new Customer(1L, "Daffy", "Duck");
     AccountNumber accountNumber1 = new AccountNumber(1L);
     Account account1 = new Account(1L, customer1, 51L, 1L, 101L, accountNumber1, new BigDecimal("100.50"));
-    atmService.transfer(account1, account1, new BigDecimal("100.50"));
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+      atmService.transfer(account1, account1, new BigDecimal("100.50"));
+    });
+    assertTrue(exception.getStatusCode().isSameCodeAs(HttpStatus.NOT_MODIFIED));
   }
 }
--- demo/src/test/java/com/example/demo/service/BankServiceTest.java
+++ demo/src/test/java/com/example/demo/service/BankServiceTest.java
@@ -6,14 +6,14 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.demo.core.account.Account;
 import com.example.demo.core.account.AccountNumber;
@@ -27,9 +27,9 @@ import com.example.demo.core.customer.CustomerService;
 import com.example.demo.core.customer.converter.CustomerToCustomerDTO;
 import com.example.demo.core.customer.web.CustomerDTO;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class BankServiceTest
+class BankServiceTest
 {
 
   @Autowired
